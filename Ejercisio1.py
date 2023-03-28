b9898adbee23d85b92f6946e066c095ccf46d10f
Rag-x <juanj.bravomax@gmail.com>
2023-03-28 14:54:51 -0500

    v4

---
+++ Ejercisio1.py
@@ -0,0 +1,35 @@
+nombre= "Juan Jose"
+apellido= "Bravo Barbecho"
+print(nombre +" "+ apellido)
+print(nombre , apellido)
+print(nombre ," ", apellido)
+#Imprime un mensaje en cadena
+#Multiplicar el nombre
+print(nombre*4)
+#funcion len numero de valores
+print(len(nombre))
+#Convercion de datos de textos
+num=int(input("ingrese numero: "))
+print(type(num))
+cadena=str(num)
+print(type(cadena))
+#Dato numerico
+x=5
+print(x)
+y=8.17
+type(y)
+#Operaciones
+numero_1=12
+numero_2=3.55
+num=numero_1 + numero_2
+print(num)
+#Tarea en clases
+m = 4
+a = 3
+
+F = m *a
+print(F)
+# Comparando dos valores 
+a = 3
+b = 's'
+a == b
