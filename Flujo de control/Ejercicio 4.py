85b62b4c782d4de41eae30ea7057e64a64f99bab
Rag-x <juanj.bravomax@gmail.com>
2023-03-30 14:51:02 -0500

    v7

---
+++ Flujo de control/Ejercicio 4.py
@@ -0,0 +1,6 @@
+# 4. Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+n = int(input("Ingrese el numero: "))
+if n%2 == 0:
+    print ("Es par")
+else:
+    print("Es impar")
