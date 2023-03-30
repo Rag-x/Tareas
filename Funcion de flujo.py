3c0b3c4491b8c3ed7ab90466d6637b94b3b9779f
Rag-x <juanj.bravomax@gmail.com>
2023-03-30 14:56:14 -0500

    v0

---
+++ Funcion de flujo.py
@@ -0,0 +1,12 @@
+#Explicacion
+a=5
+if a==5:
+    print ("El numero es igual a 5")
+#Ejercisios
+a=int(input("Ingrese un numero: "))
+if a==5:
+    print("El numero es igual a 5")
+    
+ #Funcion de else
+else  : 
+    print("El numero " ,a,"es diferente de 5")
