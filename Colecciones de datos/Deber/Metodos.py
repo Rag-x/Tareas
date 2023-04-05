8b63aaab67a0e3ce33c84d66c58de8890359651a
Rag-x <juanj.bravomax@gmail.com>
2023-04-05 14:31:51 -0500

    ve

---
+++ Colecciones de datos/Deber/Metodos.py
@@ -0,0 +1,9 @@
+#Primer metodo
+lista=[1,2,3,4,5,6,7,8,9,10]
+suma=0
+for e in lista:
+    suma=suma + e
+print (suma)
+#Segundo metodo:
+nume=[1,2,3,4,5,6,7,8,9,10]
+print ("la suma de la lista es:",sum(nume))
