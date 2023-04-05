357d2f84d21c276e10ffa337839154d9a9f5bb66
Rag-x <juanj.bravomax@gmail.com>
2023-04-05 15:06:43 -0500

    as

--- Colecciones de datos/Deber/Diccionarios .py
+++ Colecciones de datos/Deber/Diccionarios .py
@@ -14,17 +14,25 @@ print (alumnos,"\n")
 
 instituto={"carreras":["Bigdata","Mecatronica","Saiver seguridad","electricidad","Desarrollo"],
            "materias":["comunicacion","programacion","lenguaje y comunicacion","redes","sistemas"],
-           "profesores":["Marta","Adriana","Nivicela","Belen","Ana","Veronica"],
+           "profesores":["Ochoa","Adriana","Nivicela","Belen","Ana","Veronica"],
           "notas":[70,83,75,76,81,73,72]
            }
 print(instituto["carreras"])
 print(instituto["materias"])
 print(instituto["profesores"])
 print(instituto["notas"])
-m=sum(instituto["notas"])/len(instituto["notas"])
+m=round(sum(instituto["notas"])/len(instituto["notas"]))
 print ("la suma de la lista es:", m)
 promedio=0
 for e in (instituto["notas"]):
  promedio+=e
 print(promedio/len(instituto["notas"]))
+print (round(promedio/len(instituto["notas"])))
+print("Valor maximo",max(instituto["notas"]))
+nota_minima=min(instituto["notas"])
 
+posicion=((instituto["notas"]).index(nota_minima))
+
+print("La nota minima es:",nota_minima)
+print("El nombre de la materia:",instituto["materias"][0])
+print("El nombre del profesor:",instituto["profesores"][0])
