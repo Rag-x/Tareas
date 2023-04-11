93dbf6f51cf2b5f5ac93a5e2811771deec73561a
Rag-x <juanj.bravomax@gmail.com>
2023-04-11 09:48:04 -0500

    vs

---
+++ Def/menu de opciones.py
@@ -0,0 +1,39 @@
+##############MENU DE OPCIONES###################
+Opcion =1
+while opcion>=0 and opcion<=10:
+    print("############# Menu de Opciones ##############")
+    print("1.contador Ascendente")
+    print("2.contador Decendente")
+    print("3.suma de Numeros")
+    print("4.Factorial")
+    print("5.Adivinar un Numero")
+    print("6.Contador de vocales")
+    print("7.Suma de numeros pares")
+    print("8.Calculo de potencia")
+    print("9.Clculo de promedio")
+    print("10.Contador de palabras")
+    print("0.Salir")
+    opcion=int(input("Ingrese una opcion: "))
+    if opcion ==0:
+        print("Se a seleccionado la opcion salir")
+        break
+    elif opcion ==1:
+        print ("A seleccionado la opcion de contador acendente")
+    elif opcion ==2:
+        print ("A seleccionado la opcion de contador decendente")
+    elif opcion ==3:
+        print ("A seleccionado la opcion de suma de numeros")
+    elif opcion ==4:
+        print ("A seleccionado la opcion de Factorial")
+    elif opcion ==5:
+        print ("A seleccionado la opcion de adivinar un numero")
+    elif opcion ==6:
+        print ("A seleccionado la opcion de contador de vocales")
+    elif opcion ==7:
+        print ("A seleccionado la opcion de suma de numeros pares")
+    elif opcion ==8:
+        print ("A seleccionado la opcion de calculo de potencia")
+    elif opcion ==9:
+        print ("A seleccionado la opcion de calculo de promedio")
+    elif opcion ==10:
+        print ("A seleccionado la opcion de contador de palabras")
